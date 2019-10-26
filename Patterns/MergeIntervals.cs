1785903084d180e8a9fc904c5ba59ed08d08841f
mattorus <mattorus@gmail.com>
2019-10-26 12:37:59 -0700

    Add MergeIntervals pattern.

---
+++ Patterns/MergeIntervals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPatterns.Patterns
+{
+    class MergeIntervals
+    {
+        public static void RunTests()
+        {
+            int[] arr;
+            int num;
+            ListNode result;
+            string testPattern = "MERGEINTERVALS";
+            Helpers.PrintStartTests(testPattern);
+
+
+            Helpers.PrintEndTests(testPattern);
+        }
+
+    }
+}

bf438491d75220a5235b017f04971943cf99f17c
mattorus <mattorus@gmail.com>
2019-10-29 08:54:19 -0700

    Add Constants.

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPatterns
+{
+    class Constants
+    {
+        public static Comparison<int> CompareInt = (x, y) => x.CompareTo(y);
+        public static Comparison<int[]> CompareCoordX = (x, y) => x[0].CompareTo(y[0]);
+        public static Comparison<int[]> CompareCoordY = (x, y) => x[1].CompareTo(y[1]);
+
+    }
+}
